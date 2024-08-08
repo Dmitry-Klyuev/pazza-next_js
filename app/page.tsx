1d0e2bbfd0a200270ec941ac8e3d82ed7fffb36d
Dmitry-Klyuev <i6169696@gmail.com>
2024-08-08 22:27:16 +0300

    add categories on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import {Container} from "@/components/shared/container";
+import {Title} from "@/components/shared/title";
+import {Categories} from "@/components/shared/categories";
 
 export default function Home() {
-  return (
-    <div className={'w-full'}>
-      <h1>Home page</h1>
-    </div>
-  );
+    return (
+        <>
+            <Container className={'mt-10'}>
+                <Title text={'Все пиццы'} size={'lg'} className={'font-extrabold'}/>
+                <Categories/>
+            </Container>
+        </>
+    );
 }
---
+++ components/shared/categories.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import React, {useState} from "react";
+import {cn} from "@/lib/utils";
+
+interface Props {
+    className?: string
+}
+
+const cats = ["Пиццы", "Комбо", "Закуски", "Коктейли", "Кофе", "Напитки", "Десерты"];
+
+export const Categories: React.FC<Props> = ({className}) => {
+    const [activeIndex, setActiveIndex] = useState(0)
+
+
+    return (
+        <div className={cn('inline-flex gap-1 p-1 rounded-2xl bg-gray-50 ', className)}>
+            {cats.map((el, i) => (
+                <a key={i} className={cn(
+                    'flex items-center font-bold h-11 rounded-2xl px-5',
+                    activeIndex === i && 'bg-white shadow-md shadow-gray-200 text-primary'
+                )}
+                onClick={()=>setActiveIndex(i)}
+                >
+                    <button>{el}</button>
+                </a>
+            ))}
+        </div>
+    );
+};
