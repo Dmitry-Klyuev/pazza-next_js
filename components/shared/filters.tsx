f4f0eb80945f5a6a6e11d4d397b91c4774e94e47
Dmitry-Klyuev <i6169696@gmail.com>
2024-08-12 22:47:08 +0300

    started filters components

---
+++ components/shared/filters.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Title} from "@/components/shared/title";
+import {FilterCheckbox} from "@/components/shared/filter-checkbox";
+
+interface Props {
+    className?: string
+
+}
+
+export const Filters: React.FC<Props> = ({className}) => {
+    return (
+        <div>
+            <Title text={'Фильтрация'} size={'sm'} className={'mb-5 font-bold'} />
+            <div className={'flex flex-col gap-4'}>
+                <FilterCheckbox text={'Можно выбрать'} value={'1'} />
+                <FilterCheckbox text={'Новинки'} value={'2'} />
+            </div>
+
+
+        </div>
+    );
+};
