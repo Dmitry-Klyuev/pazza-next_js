42ca27ec63620dbcef7512dd7121533578c098bd
Dmitry-Klyuev <i6169696@gmail.com>
2024-09-06 20:54:57 +0300

    refactor show ingredients

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import {FilterCheckbox, FilterCheckboxProps} from "@/components/shared/filter-checkbox";
 import {cn} from "@/lib/utils";
@@ -26,24 +27,35 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
                                                           defaultValue,
                                                           className
                                                       }) => {
+
+    const [showAll, setShowAll] = React.useState<boolean>(false);
+    const showItems = showAll ? items : items.slice(0, limit);
+
     return (
         <div className={cn(className, '')}>
             <p className={'font-bold mb-3'}>{title}</p>
-            <div className={'mb-5'}>
+            {showAll && <div className={'mb-5'}>
                 <Input placeholder={searchInputPlaceholder} className={'bg-gray-50 border-none'}/>
-            </div>
-            <div className={'flex flex-col gap-4 max-h-96 pr-2 overflow-y-auto scrollbar'}>
-                {items.map((item, index) => (
+            </div>}
+
+            <div className={'flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar'}>
+                {showItems.map((item, index) => (
                     <FilterCheckbox key={index}
                                     text={item.text}
                                     value={item.value}
-                                    onChange={onChange}
                                     checked={false}
                                     endAdornment={item.endAdornment}
-
+                                    onCheckedChange={() => console.log('click')}
                     />
                 ))}
             </div>
+            {items.length > limit && (
+                <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''}>
+                    <button className={'text-primary mt-3'}
+                            onClick={()=>setShowAll(!showAll)}
+                    >{showAll ? 'скрыть' : 'показать'}</button>
+                </div>
+            )}
         </div>
     );
 };
--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -13,22 +13,101 @@ interface Props {
 export const Filters: React.FC<Props> = () => {
     return (
         <div>
-            <Title text={'Фильтрация'} size={'sm'} className={'mb-5 font-bold'} />
+            <Title text={'Фильтрация'} size={'sm'} className={'mb-5 font-bold'}/>
             <div className={'flex flex-col gap-4'}>
-                <FilterCheckbox text={'Можно выбрать'} value={'0'} />
-                <FilterCheckbox text={'Новинки'} value={'2'} />
+                <FilterCheckbox text={'Можно выбрать'} value={'0'}/>
+                <FilterCheckbox text={'Новинки'} value={'2'}/>
             </div>
             <div className={'mt-5 border-y border-gray-200 py-6 pb-7'}>
                 <p className={'font-bold mb-3'}>Цены от и до:</p>
                 <div className={'flex gap-3 mb-5'}>
-                    <Input type={'number'} placeholder={'0'} min={0} max={1000} defaultValue={0} />
-                    <Input type={'number'} min={100} max={1000} placeholder={'10000'} />
+                    <Input type={'number'} placeholder={'0'} min={0} max={1000} defaultValue={0}/>
+                    <Input type={'number'} min={100} max={1000} placeholder={'10000'}/>
 
                 </div>
                 <RangeSlider min={1} max={1000} step={10} value={[0, 1000]}/>
 
             </div>
-            <CheckboxFiltersGroup className={'py-8'} title={'Ингредиенты'} />
+            <CheckboxFiltersGroup className={'mt-5'}
+                                  title={'Ингредиенты'}
+                                  limit={6}
+                                  defaultItems={[
+                                      {
+                                          text: 'Сырный соус',
+                                          value: '1',
+                                      },
+                                      {
+                                          text: 'Моццарелла',
+                                          value: '2',
+                                      },
+                                      {
+                                          text: 'Чеснок',
+                                          value: '3',
+                                      },
+                                      {
+                                          text: 'Солённые огурчики',
+                                          value: '4',
+                                      },
+                                      {
+                                          text: 'Красный лук',
+                                          value: '5',
+                                      },
+                                      {
+                                          text: 'Томаты',
+                                          value: '6',
+                                      },
+                                  ]}
+                                  items={[
+                                      {
+                                          text: 'Сырный соус',
+                                          value: '1',
+                                      },
+                                      {
+                                          text: 'Моццарелла',
+                                          value: '2',
+                                      },
+                                      {
+                                          text: 'Чеснок',
+                                          value: '3',
+                                      },
+                                      {
+                                          text: 'Солённые огурчики',
+                                          value: '4',
+                                      },
+                                      {
+                                          text: 'Красный лук',
+                                          value: '5',
+                                      },
+                                      {
+                                          text: 'Томаты',
+                                          value: '6',
+                                      },
+                                      {
+                                          text: 'Сырный соус',
+                                          value: '1',
+                                      },
+                                      {
+                                          text: 'Моццарелла',
+                                          value: '2',
+                                      },
+                                      {
+                                          text: 'Чеснок',
+                                          value: '3',
+                                      },
+                                      {
+                                          text: 'Солённые огурчики',
+                                          value: '4',
+                                      },
+                                      {
+                                          text: 'Красный лук',
+                                          value: '5',
+                                      },
+                                      {
+                                          text: 'Томаты',
+                                          value: '6',
+                                      },
+                                  ]}
+            />
 
         </div>
     );
